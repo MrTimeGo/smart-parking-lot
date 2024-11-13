af9969d93e5638579b1b1295c4a4de78abb13cdf
BelitskyiAlexandr <71705897+BelitskyiAlexandr@users.noreply.github.com>
2024-11-13 22:31:15 +0200

    recognizer func (#8)
    * recognizer func
    
    * added photos
    
    * req + image from byte decoder

--- backend/car-plate-recognizer/recognizer.py
+++ backend/car-plate-recognizer/recognizer.py
@@ -1,2 +1,45 @@
+import cv2
+import numpy as np
+import imutils
+import easyocr
+
 def recognize_image(imageBytes):
-    return "AA1234AA"
+
+    nparr = np.frombuffer(imageBytes, np.uint8)
+
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+    if img is None:
+        print("Cannot decode the image")
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    bfilter = cv2.bilateralFilter(img, 11, 17, 17) #Noise reduction
+    edged = cv2.Canny(bfilter, 30, 200) #Edge detection
+
+    keypoints = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours = imutils.grab_contours(keypoints)
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
+
+    location = None
+    for contour in contours:
+        approx = cv2.approxPolyDP(contour, 10, True)
+        if len(approx) == 4:
+            location = approx
+            break
+
+    mask = np.zeros(gray.shape, np.uint8)
+    new_image = cv2.drawContours(mask, [location], 0,255, -1)
+    new_image = cv2.bitwise_and(img, img, mask=mask)
+
+    (x,y) = np.where(mask==255)
+    (x1, y1) = (np.min(x), np.min(y))
+    (x2, y2) = (np.max(x), np.max(y))
+    cropped_image = gray[x1:x2+1, y1:y2+1]
+
+    reader = easyocr.Reader(['en'])
+    result = reader.readtext(cropped_image)
+
+    text = result[0][-2]
+
+    return text
